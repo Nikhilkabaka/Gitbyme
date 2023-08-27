c94a6110dd2531ea40ba591c6fc0c3438d3e23ad
nikhilgowdakabaka366@gmail.com <softwarepandith@gmail.com>
2023-08-27 19:58:47 +0530

    Changed

--- Python-progs/arith.py
+++ Python-progs/arith.py
@@ -7,12 +7,11 @@ def mul(n1,n2):
 def sub(n1,n2):
     return(n1-n2)
 
-def div(n1,n2):
-    return(n1/n2)
+
 
 a=int(input("Enter num1 :"))
 b=int(input("Enter num2 :"))
 print("Ans=",add(a,b))
-print("Ans=",div(a,b))
+
 print("Ans=",mul(a,b))
 print("Ans=",sub(a,b))
