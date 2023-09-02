3a2b9e3760d1a370327c833cd33919e8a47a7442
prathambangera <prathambangera49@gmail.com>
2023-09-02 12:18:25 +0530

    division added

--- Python-progs/arith.py
+++ Python-progs/arith.py
@@ -4,11 +4,11 @@ def add(n1,n2):
 def mul(n1,n2):
     return(n1*n2)
 
-def sub(n1,n2):
-    return(n1-n2)
-
 def div(n1,n2):
     return(n1/n2)
+
+def sub(n1,n2):
+    return(n1-n2)
 
 a=int(input("Enter num1 :"))
 b=int(input("Enter num2 :"))
